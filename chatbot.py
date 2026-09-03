53dafd5f6a2f9cad2bedfffdb75530a82246d7af
mohit9618 <pwmohit1@gmail.com>
2026-09-03 22:36:34 +0530

    Completed Prompts section in Langchain

--- chatbot.py
+++ chatbot.py
@@ -2,21 +2,25 @@ import logging
 logging.disable(logging.WARNING)
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage , AIMessage , SystemMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model='gemini-3.7-flash')
 
-chat_history = []
+chat_history = [
+    SystemMessage(content = 'You are a helpful aissistant.')
+
+]
 
 while True:
     user_input = input('You: ')
-    chat_history.append(("human", user_input))
+    chat_history.append(HumanMessage(content = user_input))
     if user_input == 'exit':
         break
     result = model.invoke(chat_history)
-    chat_history.append(("ai", result.content))
+    chat_history.append(AIMessage(content = result.content))
     print("AI: ",result.content[0]['text'])
 
-print(chat_history)
+print(chat_history.content[0]['text'])
---
+++ message_placeholder.py
@@ -0,0 +1,19 @@
+from langchain_core.prompts import ChatPromptTemplate , MessagesPlaceholder
+
+# Chat Template
+chat_template = ChatPromptTemplate([
+    ('system', 'You are a helpful customer support agent'),
+    MessagesPlaceholder(variable_name='chat_history'),
+    ('human','{query}')
+])
+
+# Load Chat history
+chat_history = []
+with open('chat_history.txt') as f:
+    chat_history.extend(f.readlines())
+
+
+# Create Prompt
+prompt = chat_template.invoke({'chat_history':chat_history , 'query':'Where is my refund'})
+
+print(prompt)
