60d7776fceb2c160559ed039220d55e2f7a554dc
mohit9618 <pwmohit1@gmail.com>
2026-09-03 20:06:27 +0530

    Made reusable template and uses it in tool using chain with model.

--- prompt_ui.py
+++ prompt_ui.py
@@ -1,7 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import streamlit as st
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate , load_prompt
 
 load_dotenv()
 st.header('Research Tool')
@@ -26,36 +26,16 @@ length_input = st.selectbox(
     ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"]
 )
 
-template  = PromptTemplate(
-    template = """
-    Please summarize the research paper titled "{paper_input}" with the following specifications:
-    
-    Explanation Style: {style_input}
-    Explanation Length: {length_input}
-    
-    1. Mathematical Details:
-       - Include relevant mathematical equations if present in the paper.
-       - Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-    
-    2. Analogies:
-       - Use relatable analogies to simplify complex ideas.
-    
-    If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-    
-    Ensure the summary is clear, accurate, and aligned with the provided style and length.
-    """,
-    input_variables=['paper_input' , 'style_input' , 'length_input']
+template  = load_prompt('template.json')
 
-)
-
-prompt = template.invoke({
-            'paper_input':paper_input,
-            'style_input':style_input,
-            'length_input':length_input
-         })
 
 model  = ChatGoogleGenerativeAI(model='gemini-3.7-flash')
 
 if st.button('Summarize'):
-    result = model.invoke(prompt)
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
     st.write(result.content[0]["text"]) 
