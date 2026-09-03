6cb7cbf729f12cc6c8d0b4d8db9f6ee7e09a04e2
mohit9618 <pwmohit1@gmail.com>
2026-09-03 20:29:05 +0530

    Added history feature in chatbot

--- chatbot.py
+++ chatbot.py
@@ -8,9 +8,15 @@ load_dotenv()
 
 model = ChatGoogleGenerativeAI(model='gemini-3.7-flash')
 
+chat_history = []
+
 while True:
     user_input = input('You: ')
+    chat_history.append(("human", user_input))
     if user_input == 'exit':
         break
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    chat_history.append(("ai", result.content))
     print("AI: ",result.content[0]['text'])
+
+print(chat_history)
